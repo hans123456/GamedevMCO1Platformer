330cb92fc20830724369e40a7933325564459a3f
hans123456 <h.g.n.chua@gmail.com>
2016-03-30 03:15:59 +0800

    can't do it

--- Assets/Scripts/LeftScript.cs
+++ Assets/Scripts/LeftScript.cs
@@ -13,7 +13,7 @@ public class LeftScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Input.touchCount > 0 || Input.GetKey(KeyCode.LeftArrow)) {
-			player.transform.Translate (-Vector2.left * 4f * Time.deltaTime);
+			player.transform.Translate (Vector2.right * 4f * Time.deltaTime);
 			player.transform.eulerAngles = new Vector2 (0, 180);
 		}
 	}
