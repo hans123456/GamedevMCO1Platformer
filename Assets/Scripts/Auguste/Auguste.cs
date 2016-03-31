fdaaf15bcd1467d13b53504975f469f2bbf08f81
hans123456 <h.g.n.chua@gmail.com>
2016-04-01 03:54:46 +0800

    auguste projectile done

---
+++ Assets/Scripts/Auguste/Auguste.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Auguste : MonoBehaviour {
+
+	public float moveSpeed;
+
+	public bool attacking = false;
+	private GameObject theaObject;
+
+	private float attackTimer = 0;
+	private float attackCD = 0.3f;
+
+	private Animator anim;
+
+	public bool goLeft = false;
+	public bool goRight = false;
+	public bool attack = false;
+
+	public GameObject TheaProjectile;
+	public bool facingRight = true;
+
+	public int hitPoints;
+
+	// Use this for initialization
+	void Start () {
+		theaObject = gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (hitPoints <= 0) {
+			Destroy (gameObject);
+		}
+
+		if (attack && !attacking) {
+			attacking = true;
+			attackTimer = attackCD;
+			GameObject projectile = Instantiate (TheaProjectile);
+			projectile.GetComponentInParent<TheaProjectileScript> ().facingRight = facingRight;
+			if (facingRight) {
+				projectile.transform.position = transform.position;
+				projectile.transform.localEulerAngles = new Vector2 (0, 0);
+			} else if (!facingRight) {
+				projectile.transform.position = transform.position;	
+				projectile.transform.localEulerAngles = new Vector2 (0, 180);
+			}
+			projectile.GetComponentInParent<TheaProjectileScript> ().start = true;
+		}
+
+		if (attacking) {
+			if (attackTimer > 0) {
+				attackTimer -= Time.deltaTime;
+			} else {
+				attacking = false;
+				attackTimer = 0;
+			}
+		}
+
+		GetComponent<Animator> ().SetBool ("Attacking", attacking);
+
+		if (!attacking) {
+			if (goRight) {
+				theaObject.transform.Translate (Vector2.right * moveSpeed * Time.deltaTime);
+				theaObject.transform.localEulerAngles = new Vector2 (0, 0);
+				theaObject.GetComponent<Animator> ().SetFloat ("Speed", Mathf.Abs (1.0f));
+				facingRight = true;
+			} else if (goLeft) {
+				theaObject.transform.Translate (-Vector2.left * moveSpeed * Time.deltaTime);
+				theaObject.transform.localEulerAngles = new Vector2 (0, 180);
+				theaObject.GetComponent<Animator> ().SetFloat ("Speed", Mathf.Abs (1.0f));
+				facingRight = false;
+			} else {
+				theaObject.GetComponent<Animator> ().SetFloat ("Speed", 0);
+			}
+		}
+
+		goLeft = goRight = attack = false;
+
+	}
+
+	void Damage (int dmg) {
+		Debug.Log ("Damage");
+		hitPoints -= dmg;
+	}
+
+}
---
+++ Assets/Scripts/Auguste/AugusteProjectileScript.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AugusteProjectileScript : MonoBehaviour {
+
+	private float startPosX;
+	public bool start = false;
+	public bool facingRight = true;
+	public float distanceBeforeDestroy;
+	public float moveSpeed;
+	private GameObject playerObject;
+	private Vector3 direction;
+
+	// Use this for initialization
+	void Start () {
+		startPosX = transform.position.x;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			start = true;
+		}
+
+		if (!start) {
+
+			playerObject = GameObject.FindGameObjectWithTag ("Player");
+
+			// https://www.reddit.com/r/Unity2D/comments/2cp8ly/how_do_i_make_a_projectile_fire_in_the_direction/
+			direction = transform.position - playerObject.transform.position;
+			transform.rotation = Quaternion.LookRotation(transform.forward, direction);
+
+			return;
+		}
+
+		if (Mathf.Abs (startPosX - transform.position.x) >= distanceBeforeDestroy) {
+			Destroy (gameObject);
+		} else {
+			Vector3 velocity = gameObject.transform.rotation * Vector3.down;
+			gameObject.GetComponent<Rigidbody2D> ().AddForce(velocity * moveSpeed);
+		}
+
+	}
+
+	void OnTriggerEnter2D(Collider2D col) {
+		if (col.tag.Equals ("Player")) {
+			col.SendMessageUpwards ("Damage", 1);
+			Destroy (gameObject);
+		}
+		if (col.tag.Equals ("Ground")) {
+			Destroy (gameObject);		
+		}
+	}
+
+}
