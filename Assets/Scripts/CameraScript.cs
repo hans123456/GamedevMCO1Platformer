5d361501c7ae4930707e4cf383422d495d16643b
Chung <chung.anna@outlook.com>
2016-03-31 14:25:18 +0800

    follow camera, need to fix on screen buttons

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+    
+    public GameObject cameraObj;
+    public Vector3 specificVector;
+    public float smoothSpeed;
+ 
+    void Start () { 
+        cameraObj = GameObject.FindGameObjectWithTag("MainCamera");
+    }
+ 
+    void Update () {
+        specificVector = new Vector3(transform.position.x, transform.position.y, cameraObj.transform.position.z);
+        cameraObj.transform.position = Vector3.Lerp(cameraObj.transform.position, specificVector, smoothSpeed * Time.deltaTime);
+    }	
+}
